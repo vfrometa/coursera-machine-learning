ac1d3185138a8156cbf76fc0cec52fe8f7d055cc
Vladimir Frometa <vfrometa@gmail.com>
2017-03-13 09:48:36 -0400

    Week 9 Completed!

--- Ex8/cofiCostFunc.m
+++ Ex8/cofiCostFunc.m
@@ -11,7 +11,6 @@ X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
 
-            
 % You need to return the following values correctly
 J = 0;
 X_grad = zeros(size(X));
@@ -40,20 +39,26 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+%vfrometa debugging
+%displayVarSize(params, 'params');
+%displayVarSize(X, 'X');
+%displayVarSize(Theta, 'Theta');
+%displayVarSize(R, 'R');
+%displayVarSize(Theta_grad, 'Theta_grad');
+%displayVarSize(X_grad, 'X_grad');
+%fprintf('Cost J = %f\n', J);
+%fprintf('Stop here\n');
+%pause;
 
+J = (1/2) * sum(sum((((X * Theta') - Y) .^2) .* R));
+X_grad = ((X * Theta' - Y) .* R) * Theta;
+Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
-
-
-
-
-
-
-
-
-
-
-
-
+% Regularization
+reg = (lambda/2) * (sum(sum(Theta .^2)) + sum(sum(X .^2)));
+J += reg;
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
 
 % =============================================================
 
